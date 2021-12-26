9da486d5ec6d9db89b42e79a950a60fee8e9a29f
gek <detronics52@gmail.com>
2021-12-26 22:35:32 +0300

    НАстраиваю связь с ардуиной

--- arduino_test.py
+++ arduino_test.py
@@ -1,21 +1,100 @@
-import time
+# import serial
+
+
+# port = serial.Serial(port='/dev/ttyUSB0', baudrate=115200)
+# portList = []
+# ports = serial.availablePorts()
+# for port in ports:
+#     portList.append(port.portName())
+
+
+
 import serial
-import datetime
 
-ser = serial.Serial(port='COM6', baudrate=115200)
-received = []
+ser=serial.Serial(port='/dev/ttyUSB0',baudrate=115200)
 
-ser.write(b'begin\n')
-time.sleep(5)
+while True:
+    rx = ser.readline()
+    rxs = str(rx, 'utf-8').strip()
+    data = rxs.split(',')
+    print (data)
 
-start = datetime.datetime.now()
-for i in range(1, 100):
-    ser.write(b'test%i\r\n' % i)
-    time.sleep(0.005)
-    while ser.inWaiting() > 0:
-        line = ser.readline()
-        if line:
-            received.append(line.decode().strip())
 
-print(datetime.datetime.now() - start)
-print(received)
+
+
+
+
+
+
+# def onRead():
+    # if not serial.canReadLine(): return     # выходим если нечего читать
+    # rx = port.readline()
+    # rxs = str(rx, 'utf-8').strip()
+    # data = rxs.split(',')
+    # print(data)
+    # if data[0] == '0':
+    #     ui.lcdN.display(data[1])
+    #     ui.tempB.setValue(int(float(data[3]) * 10))
+    #     ui.tempL.setText(data[3])
+    #     global listX
+    #     global listY
+    #     listY = listY[1:]
+    #     listY.append(int(data[2]))
+    #     ui.graph.clear()
+    #     ui.graph.plot(listX, listY)
+    #
+    # if data[0] == '1':
+    #     if data[1] == '0':
+    #         ui.circle.setChecked(True)
+    #     else:
+    #         ui.circle.setChecked(False)
+    #
+    # if data[0] == '2':
+    #     global posX
+    #     global posY
+    #     posX += int((int(data[1]) - 512) / 100)
+    #     posY += int((int(data[2]) - 512) / 100)
+    #     ui.circle.setGeometry(posX, posY, 20, 20)
+
+# onRead()
+
+# def onOpen():
+#     serial.setPortName(ui.comL.currentText())
+#     serial.open(QIODevice.ReadWrite)
+
+#
+# def serialSend(data):
+#     txs = ""
+#     for val in data:
+#         txs += str(val)
+#         txs += ','
+#     txs = txs[:-1]
+#     txs += ';'
+#     serial.write(txs.encode())
+#
+#
+# def onClose():
+#     serial.close()
+#
+#
+# def ledControl(val):
+#     if val == 2: val = 1;
+#     serialSend([0, val])
+#
+#
+# def fanControl(val):
+#     if val == 2: val = 1;
+#     serialSend([3, val])
+#
+#
+# def bulbControl(val):
+#     if val == 2: val = 1;
+#     serialSend([4, val])
+#
+#
+# def servoControl(val):
+#     serialSend([2, val])
+#
+#
+#
+# serial.readyRead.connect(onRead)
--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import serial
 import tkinter as tk
+import threading
 from tkinter import messagebox as mb
+import serial.tools.list_ports
 
-port = serial.Serial(port='COM6', baudrate=9600)
-print (port.readline())
+
+port = serial.Serial( baudrate=115200)
+
 class Tasks(tk.Frame):
     def __init__(self, main):
         super().__init__()
         self.main = main
+        thread = threading.Thread(target=self.onRead, daemon=True)
+        self.data = ['1', '2', '3', '0', ]
         self.init_main()
-        self.pressure_inlet = tk.Label(bg='black',fg='green', font=('times', 12), justify=tk.LEFT, anchor='nw',text='0.00')
-        self.pressure_outlet = tk.Label(bg='black',fg='green', font=('times', 12), justify=tk.LEFT, anchor='nw',text='0.00')
-        self.regulator_position = tk.Label(bg='black',fg='green', font=('times', 12), justify=tk.LEFT, anchor='nw', text='0.0')
+        thread.start()
+        self.pressure_inlet = tk.Label(bg='black', fg='green', font=('times', 12), justify=tk.LEFT, anchor='nw',
+                                       text='0.00')
+        self.pressure_outlet = tk.Label(bg='black', fg='green', font=('times', 12), justify=tk.LEFT, anchor='nw',
+                                        text='0.00')
+        self.regulator_position = tk.Label(bg='black', fg='green', font=('times', 12), justify=tk.LEFT, anchor='nw',
+                                           text='0.0')
         self.pressure_inlet.place(x=260, y=325)
         self.pressure_outlet.place(x=755, y=325)
         self.regulator_position.place(x=525, y=325)
@@ -19,15 +28,18 @@ class Tasks(tk.Frame):
     def init_main(self):
         self.background = tk.PhotoImage(file='images/Back.png')
         background_label = tk.Label(image=self.background)
-        background_label.place(x=0,y=0)
-        p_min = tk.Label(font=('times', 12), bg='black',fg='green',justify=tk.LEFT, anchor='nw', text='0.00')
-        p_max = tk.Label(font=('times', 12), bg='black',fg='green',justify=tk.LEFT, anchor='nw', text='0.00')
-        p_value = tk.Label(font=('times', 12), bg='black',fg='green',justify=tk.LEFT, anchor='nw', text='0.00')
-        b_open = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Открыть регулятор')
-        b_close = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Закрыть регулятор')
-        b_pmin = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Редактировать',command=self.input_box)
-        b_pmax = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Редактировать',command=self.input_box)
-        b_new_value = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Редактировать',command=self.input_box)
+        background_label.place(x=0, y=0)
+        p_min = tk.Label(font=('times', 12), bg='black', fg='green', justify=tk.LEFT, anchor='nw', text=self.data[1])
+        p_max = tk.Label(font=('times', 12), bg='black', fg='green', justify=tk.LEFT, anchor='nw', text=self.data[0])
+        p_value = tk.Label(font=('times', 12), bg='black', fg='green', justify=tk.LEFT, anchor='nw', text=self.data[2])
+        b_open = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Открыть регулятор',
+                           command=lambda: port.write('0,0;'.encode()))
+        b_close = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Закрыть регулятор',
+                            command=lambda: port.write('0,1;'.encode()))
+        b_pmin = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Редактировать', command=self.input_box)
+        b_pmax = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Редактировать', command=self.input_box)
+        b_new_value = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Редактировать',
+                                command=self.input_box)
         b_start_rp = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Старт', width='5')
         b_stop_rp = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Стоп', width='5')
         b_open.place(x=950, y=25)
@@ -36,10 +48,10 @@ class Tasks(tk.Frame):
         b_pmax.place(x=190, y=142)
         b_new_value.place(x=190, y=89)
         b_start_rp.place(x=490, y=150)
-        b_stop_rp.place(x=550,y=150)
+        b_stop_rp.place(x=550, y=150)
         p_min.place(x=80, y=35)
-        p_max.place(x=80,y=142)
-        p_value.place(x=80,y=89)
+        p_max.place(x=80, y=142)
+        p_value.place(x=80, y=89)
 
     def input_box(self):
         self.window = tk.Toplevel()
@@ -49,9 +61,10 @@ class Tasks(tk.Frame):
         self.window.title('Установка нового значения давления')
         Description = tk.Label(self.window, text='Введите новое значение давления')
         Description.place(x=30, y=50)
-        self.new_value = tk.Entry(self.window, width=5, background='white',)
+        self.new_value = tk.Entry(self.window, width=5, background='white', )
         self.new_value.place(x=130, y=90)
-        confirmation = tk.Button(self.window, bg='#e6e7e4', bd=0, activebackground='#636362', text='OK', command=self.close_window)
+        confirmation = tk.Button(self.window, bg='#e6e7e4', bd=0, activebackground='#636362', text='OK',
+                                 command=self.close_window)
         confirmation.place(x=130, y=120)
 
     def close_window(self):
@@ -63,11 +76,25 @@ class Tasks(tk.Frame):
         elif float(self.new_value.get()) > 7.5:
             mb.showerror("Ошибка", "Значение должно быть меньше 7,5 кгс/см2")
 
+    def onRead(self):
+
+        port.setPort(port='/dev/ttyUSB0')
+        port.open()
+        while True:
+            rx = port.readline()
+            rxs = str(rx, 'utf-8').strip()
+            self.data = rxs.split(',')
+            print(data)
+
+
+
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    app = Tasks(root)
-    root.title('Главное окно')
-    root.geometry('1095x400')
-    root.resizable(False, False)
-    root.mainloop()
+    while True:
+        root = tk.Tk()
+        app = Tasks(root)
+        root.title('Главное окно')
+        root.geometry('1095x400')
+        root.resizable(False, False)
+        root.mainloop()
+
