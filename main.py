871a05fbfe1f09b6e271b4e14adec77b6c828e70
Konstantin <detronics52@gmail.com>
2022-02-08 16:15:31 +0300

    Написал функции для кнопок, настроил блокировку кнопок и контроль выбора порта

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import serial
 import tkinter as tk
 from tkinter import ttk
-import threading
 from tkinter import messagebox as mb
 import serial.tools.list_ports
 
@@ -31,34 +30,30 @@ class Tasks(tk.Frame):
         background_label = tk.Label(image=self.background)
         background_label.place(x=0, y=0)
         p_min = tk.Label(font=('times', 12), bg='black', fg='grey', justify=tk.LEFT, anchor='nw', text=self.data[1])
-        # p_max = tk.Label(font=('times', 12), bg='black', fg='grey', justify=tk.LEFT, anchor='nw', text=self.data[0])
         p_value = tk.Label(font=('times', 12), bg='black', fg='grey', justify=tk.LEFT, anchor='nw', text=self.data[2])
         choose_port = tk.Label(font=('times', 12), justify=tk.LEFT,bg='#476B8F',fg='white', anchor='nw', text='Выберите порт')
         self.ports = ttk.Combobox(values=avaible_ports, width=11)
-        b_connect = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Подключить порт',
+        self.b_connect = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Подключить порт',
                               command=self.connect_port)
-        b_open = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Открыть регулятор',
-                           command=lambda: port.write('5;'.encode()))
-        b_close = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Закрыть регулятор',
-                            command=lambda: port.write('6;'.encode()))
+        self.b_open = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Открыть регулятор',state='disabled',
+                           command=self.open_reg)
+        self.b_close = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Закрыть регулятор',state='disabled',
+                            command=self.close_reg)
         b_pmin = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Редактировать', command=self.input_box)
-        # b_pmax = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Редактировать', command=self.input_box)
         b_new_value = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Редактировать',
                                 command=self.input_box)
-        b_start_rp = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Старт', width='5')
-        b_stop_rp = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Стоп', width='5')
-        b_connect.place(x=485, y=80)
+        self.b_start_rp = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Старт',state='disabled', width='5',command=self.start_rp)
+        self.b_stop_rp = tk.Button(bg='#e6e7e4', bd=0, activebackground='#636362', text='Стоп',state='disabled', width='5',command=self.stop_rp)
+        self.b_connect.place(x=485, y=80)
         self.ports.place(x=492, y=55)
-        b_open.place(x=950, y=25)
-        b_close.place(x=950, y=60)
+        self.b_open.place(x=950, y=25)
+        self.b_close.place(x=950, y=60)
         choose_port.place(x=485, y=25)
         b_pmin.place(x=190, y=35)
-        # b_pmax.place(x=190, y=142)
         b_new_value.place(x=190, y=89)
-        b_start_rp.place(x=490, y=150)
-        b_stop_rp.place(x=550, y=150)
+        self.b_start_rp.place(x=490, y=150)
+        self.b_stop_rp.place(x=550, y=150)
         p_min.place(x=80, y=35)
-        # p_max.place(x=80, y=142)
         p_value.place(x=80, y=89)
 
     def input_box(self):
@@ -84,10 +79,37 @@ class Tasks(tk.Frame):
         elif float(self.new_value.get()) > 7.5:
             mb.showerror("Ошибка", "Значение должно быть меньше 7,5 кгс/см2")
 
+    def start_rp(self):
+        self.b_start_rp.config(state='disabled')
+        self.b_stop_rp.config(state='normal')
+        port.write('3;'.encode())
+
+    def stop_rp(self):
+        self.b_start_rp.config(state='normal')
+        self.b_stop_rp.config(state='disabled')
+        port.write('4;'.encode())
+    #     TODO сделать блокировку кнопок если арудуино будет зависать
+
+    def open_reg(self):
+        self.b_open.config(state='disabled')
+        port.write('5;'.encode())
+        self.b_open.config(state='normal')
+
+    def close_reg(self):
+        self.b_close.config(state='disabled')
+        port.write('6;'.encode())
+        self.b_close.config(state='normal')
+
     def connect_port(self):
-        port.setPort(port=self.ports.get())
-        port.open()
-        self.onRead()
+        if not self.ports.get():
+            mb.showerror("Ошибка", "Порт не выбран")
+        else:
+            port.setPort(port=self.ports.get())
+            port.open()
+            self.b_start_rp.config(state='normal')
+            self.b_open.config(state='normal')
+            self.b_close.config(state='normal')
+            self.onRead()
 
     def onRead(self):
         while True:
